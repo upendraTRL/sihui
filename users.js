cf24f5fa2daf7dbf9d57635605875941ce60602b
upendraTRL <upendrataral21@gmail.com>
2022-08-25 21:25:13 +0530

    update

--- users.js
+++ users.js
@@ -1,18 +1,21 @@
 const mongoose = require("mongoose");
 
 let userSchema = new mongoose.Schema({
-    _id:mongoose.Schema.Types.ObjectId,
+    _id: mongoose.Schema.Types.ObjectId,
     email_id: String,
     password: String,
-    reg_name:String,
-    city:String,
-    state:String,
+    reg_name: String,
+    city: String,
+    state: String,
     map_data: String,
     charts_data: String,
     cala_n: [String],
     chart_lab: String,
-    githublink: String
+    githublink: String,
+    reg_color: String,
+    reg_label: String,
+    reg_data1: String
 
 })
 
-module.exports = mongoose.model("users",userSchema);
+module.exports = mongoose.model("users", userSchema);
